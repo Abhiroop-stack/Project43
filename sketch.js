825e0fbce65e36121b2dda56e0a35cac25b32b9d
Abhiroop-stack <72975864+Abhiroop-stack@users.noreply.github.com>
2021-02-21 15:22:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+hr = hour();
+mn = minute();
+sc = second();
+
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(255,255,255);  
+ 
+  push();
+  rotate(scAngle);
+  stroke(255,0,0);
+  strokeWeight(7);
+  line(0,0,100,0);
+
+  drawSprites();
+}
